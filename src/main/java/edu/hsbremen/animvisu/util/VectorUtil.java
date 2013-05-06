b7fc02cf87ff12a720255a26ff1c8e4dc01fbe4e
Svenja <Svenja@172.31.10.116>
2013-05-06 17:44:13 +0200

    fixed createNormal

--- src/main/java/edu/hsbremen/animvisu/util/VectorUtil.java
+++ src/main/java/edu/hsbremen/animvisu/util/VectorUtil.java
@@ -9,8 +9,8 @@ public class VectorUtil {
 		Vector3f temp2 = new Vector3f();
 		Vector3f temp3 = new Vector3f();
 		
-		Vector3f.sub(p1, p2, temp1);
-		Vector3f.sub(p1, p3, temp2);
+		Vector3f.sub(p2, p1, temp1);
+		Vector3f.sub(p3, p1, temp2);
 		Vector3f.cross(temp1, temp2, temp3);
 		temp3.normalise();
 		return temp3;
